1c5ed94b2321543111b0a3bb549f11725e2403e7
thenrynadai-oss <thenrynadai@gmail.com>
2026-02-10 13:04:34 -0300

    Add files via upload

---
+++ firebase.config.example.js
@@ -0,0 +1,9 @@
+
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
---
+++ firebase.config.js
@@ -0,0 +1,1 @@
+window.FIREBASE_CONFIG = null;
